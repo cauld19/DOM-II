4cadd588b7e1991e0314daf1c7dec7d4ac0fd5f0
cauld19 <50849710+cauld19@users.noreply.github.com>
2019-09-10 16:42:26 -0700

    completed stretch

--- js/index.js
+++ js/index.js
@@ -90,8 +90,9 @@ console.log(footer)
 /* stop propogation */
 
 let destinationContent = document.querySelector('.content-destination');
-destinationContent.addEventListener('click', () => {
-    destinationContent.style.background = 'grey'
+destinationContent.addEventListener('click', (e) => {
+    destinationContent.style.background = '#437f88';
+    destinationContent.style.transition = 'background 0.5s';
 })
 
 let destinationContentPara = document.querySelector('.content-destination h2');
@@ -99,6 +100,17 @@ destinationContentPara.addEventListener('click', function (e) {
     e.stopPropagation();
     console.log('h2 was pressed')
 });
+
+let advenAwaits = document.querySelector('.content-section');
+advenAwaits.addEventListener('click', (event) => {
+     if (event.target.classList.contains('advenAwaits')) {
+        advenAwaits.style.background = '#437f88';
+        advenAwaits.style.transition = 'background 0.5s';
+        console.log('was clicked')
+    }
+})
+
+
 
 /* prevent default */
 
@@ -111,3 +123,12 @@ navLink.forEach((item) => {
 })
 console.log(navLink)
 
+/* greensock */
+
+TweenMax.to('.logo-heading', 2, { x: 300, rotation: 360, scale: 0.5 });
+TweenMax.staggerFrom('img', 2, {opacity: 0, y: 500, rotation: 360, delay: 0.5, onComplete: complete });
+
+
+function complete() {
+    alert('Tweens complete!')
+}
